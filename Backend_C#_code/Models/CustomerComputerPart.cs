bf7fad7867d97d21eead834fba1071898a747977
Ardi Grapci <ardiehnberggrapci@gmail.com>
2022-03-25 10:40:05 +0100

    Updatede some models

---
+++ Backend_C#_code/Models/CustomerComputerPart.cs
@@ -0,0 +1,9 @@
+namespace Backend_C__code.Models
+{
+    public class CustomerComputerPart
+    {
+        public int CustomerId { get; set; }
+
+        public int ComputerPartId { get; set; }
+    }
+}
---
+++ Backend_C#_code/Models/CustomerLaptop.cs
@@ -0,0 +1,9 @@
+namespace Backend_C__code.Models
+{
+    public class CustomerLaptop
+    {
+        public int CustomerId { get; set; }
+
+        public int LaptopId { get; set; }
+    }
+}
