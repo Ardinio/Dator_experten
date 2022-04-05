4e4c88639ecdcde41917c5ca6becb58dfe0de306
Ardi Grapci <ardiehnberggrapci@gmail.com>
2022-04-05 09:07:26 +0200

    Added routing

---
+++ webbside/src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h3>
+        Dator Experten
+      </h3>
+    </div>
+  );
+}
+
+export default App;
