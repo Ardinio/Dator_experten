95a92713269cf7afac6226598d5e236e03815bc1
Ardi Grapci <ardiehnberggrapci@gmail.com>
2022-05-31 10:26:33 +0200

    Added some wellcome text

--- webbside/src/pages/HomePage.tsx
+++ webbside/src/pages/HomePage.tsx
@@ -1,10 +1,13 @@
-import React, {Component} from "react";
 import { Container } from "@mui/material";
+import { Typography } from "@mui/material"
 
 const HomePage = () => {
     return (
         <Container sx={{marginY: 20}}>
             This is Home Page
+            <Typography>
+                Välkommen till dator experten allt för att enkelt hitta eller bygga en dator.
+            </Typography>
         </Container>
     )
 }
